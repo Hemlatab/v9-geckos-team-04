d3ae36ea9358731fe67d55012fe1cd4c237f305b
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-06-23 15:20:55 +0400

    Updates

---
+++ precache-manifest.33326b3655dd0d7f232b7071c9b63a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e278400c92a2344a98ad3dbbf09bb4e",
+    "url": "/v9-geckos-team-04/index.html"
+  },
+  {
+    "revision": "dfe33be32b82e2bf4d19",
+    "url": "/v9-geckos-team-04/static/css/main.5b0c8aeb.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/v9-geckos-team-04/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "dfe33be32b82e2bf4d19",
+    "url": "/v9-geckos-team-04/static/js/main.cd93fbdc.chunk.js"
+  },
+  {
+    "revision": "22abbc862262d027999e",
+    "url": "/v9-geckos-team-04/static/js/runtime~main.f63ebc68.js"
+  },
+  {
+    "revision": "04f03d2891d24fa647d288a7f6759c20",
+    "url": "/v9-geckos-team-04/static/media/speech_to_text.04f03d28.svg"
+  }
+]);
